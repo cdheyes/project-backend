e5832dcf88a77d9a69820f47f17579076aac5f7d
Linden Hamer <linden.hamer.97@hotmail.co.uk>
2022-05-27 15:06:23 +0100

    find functions for specific restaurants based on cuisines

--- src/restaurants/restController.js
+++ src/restaurants/restController.js
@@ -20,3 +20,64 @@ exports.listRest =  async (req, res) => {
       res.status(500).send({ error: error.message })
   }
 };
+
+// list all american restaurants
+exports.listRestAmerican =  async (req, res) => {
+  try {
+      const restaurantsAmerican = await Restaurants.find({cuisine: "American"});
+      res.status(200).send({ restaurantsAmerican })
+  } catch (error) {
+      console.log(error);
+      res.status(500).send({ error: error.message })
+  }
+};
+// list all indian restaurants
+exports.listRestIndian =  async (req, res) => {
+  try {
+      const restaurantsIndian = await Restaurants.find({cuisine: "Indian"});
+      res.status(200).send({ restaurantsIndian })
+  } catch (error) {
+      console.log(error);
+      res.status(500).send({ error: error.message })
+  }
+};
+// list all chinese restaurants
+exports.listRestChinese =  async (req, res) => {
+  try {
+      const restaurantsChinese = await Restaurants.find({cuisine: "Chinese"});
+      res.status(200).send({ restaurantsChinese })
+  } catch (error) {
+      console.log(error);
+      res.status(500).send({ error: error.message })
+  }
+};
+// list all Italian restaurants
+exports.listRestItalian =  async (req, res) => {
+  try {
+      const restaurantsItalian = await Restaurants.find({cuisine: "Italian"});
+      res.status(200).send({ restaurantsItalian })
+  } catch (error) {
+      console.log(error);
+      res.status(500).send({ error: error.message })
+  }
+};
+// list all Turkish restaurants
+exports.listRestTurkish =  async (req, res) => {
+  try {
+      const restaurantsTurkish = await Restaurants.find({cuisine: "Turkish"});
+      res.status(200).send({ restaurantsTurkish })
+  } catch (error) {
+      console.log(error);
+      res.status(500).send({ error: error.message })
+  }
+};
+// list all Japanese restaurants
+exports.listRestJapanese =  async (req, res) => {
+  try {
+      const restaurantsJapanese = await Restaurants.find({cuisine: "Japanese"});
+      res.status(200).send({ restaurantsJapanese })
+  } catch (error) {
+      console.log(error);
+      res.status(500).send({ error: error.message })
+  }
+};
