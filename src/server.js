1ba799f5372cb04742dc40a9872661103a0d7b63
Linden Hamer <linden.hamer.97@hotmail.co.uk>
2022-05-27 11:53:06 +0100

    changes to server.js

--- src/server.js
+++ src/server.js
@@ -2,8 +2,8 @@ require("./db/connection");
 const express = require("express");
 const cors = require("cors");
 
-
 const userRouter = require("./user/userRoutes");
+const restRouter = require("./restaurants/restRoutes");
 
 const app = express();
 const port = process.env.PORT || 5001;
@@ -11,9 +11,11 @@ const port = process.env.PORT || 5001;
 app.use(express.json());
 app.use(cors());
 
-
 app.use(userRouter);
+app.use(restRouter);
 
 app.listen(port, () => {
     console.log(`Listening on port: ${port}`);
 })
+
+// server.js test push
