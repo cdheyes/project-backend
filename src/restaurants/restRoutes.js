70336b212cd4d0a3ec3f8a766c5b05e4d32d2c99
Linden Hamer <linden.hamer.97@hotmail.co.uk>
2022-05-30 17:03:06 +0100

    re-factored filter function

--- src/restaurants/restRoutes.js
+++ src/restaurants/restRoutes.js
@@ -1,7 +1,7 @@
 // require Router
 const { Router } = require("express");
 // require functions from userController
-const { addRest, listRest, listRestAmerican, listRestItalian, listRestIndian, listRestChinese, listRestTurkish, listRestJapanese} = require("./restController");
+const { addRest, findCuisine} = require("./restController");
 
 // set router fucntion in variable 
 const restRouter = Router();
@@ -10,21 +10,26 @@ const restRouter = Router();
 
 // route for adding user
 restRouter.post("/rest", addRest);
-// route for lsiting users
-restRouter.get("/rest", listRest);
 
-// route for listing american rest
-restRouter.get("/rest/american", listRestAmerican);
-// route for listing maerican rest
-restRouter.get("/rest/italian", listRestItalian);
-// route for listing indian rest
-restRouter.get("/rest/indian", listRestIndian);
-// route for listing Chinese rest
-restRouter.get("/rest/chinese", listRestChinese);
-// route for listing turkish rest
-restRouter.get("/rest/turkish", listRestTurkish);
-// route for listing japanese rest
-restRouter.get("/rest/japanese", listRestJapanese);
+// filter unction by cuisine
+restRouter.get("/rest/:cuisine", findCuisine);
+
+// old code 
+
+// route for lsiting users
+// restRouter.get("/rest", listRest);
+// // route for listing american rest
+// restRouter.get("/rest/american", listRestAmerican);
+// // route for listing maerican rest
+// restRouter.get("/rest/italian", listRestItalian);
+// // route for listing indian rest
+// restRouter.get("/rest/indian", listRestIndian);
+// // route for listing Chinese rest
+// restRouter.get("/rest/chinese", listRestChinese);
+// // route for listing turkish rest
+// restRouter.get("/rest/turkish", listRestTurkish);
+// // route for listing japanese rest
+// restRouter.get("/rest/japanese", listRestJapanese);
 
 // export 
 module.exports = restRouter;
